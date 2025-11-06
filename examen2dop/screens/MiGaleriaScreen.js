3dc6002ec68c7c28b2f81e8e565579e86b1fb369
Patito3000 <124050154@upq.edu.mx>
2025-11-06 15:31:52 -0600

    Ultimo commit
    hola

--- examen2dop/screens/MiGaleriaScreen.js
+++ examen2dop/screens/MiGaleriaScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, Platform, Alert} from 'react-native'
 import React, { useState } from 'react'
 import { Button, ImageBackground, ScrollView } from 'react-native-web';
 
@@ -17,29 +17,26 @@ export default function MiGaleriaScreen() {
 
 
     const mostrarAlerta = () => {
-        if(detalles.trim() === 'web'){
-        alert('Descripcion')
-        }
-        else{
-            Alert.alert(
-                'Hola'
-            )
-        }
+        alert('Vas a ver descripcion de las imagenes');
     }
    
 
     return(
-        <View>
+        <View style={styles.cuerpo}>
             <View style={styles.separacion}>
                 <ImageBackground
+                    title= 'Una imagen'
                     source={img1}
                     rezideMode= 'cover'
-                    imageStyle={styles.splashImage}
-                    style={styles}
+                    imageStyle={styles.image}
+                    style={styles.splashImage}
                 >
                  
                 </ImageBackground>
+             
+            </View>
 
+            <View style={styles.separacion}>
                 <ImageBackground
                     source={img2}
                     rezideMode= 'cover'
@@ -48,7 +45,97 @@ export default function MiGaleriaScreen() {
                 >
                     <Button title='Ver detalles' onPress={mostrarAlerta}/>
                 </ImageBackground>
+
+                <View style={styles.descripcion}>
+                    <Text>Descripcion descriptible</Text>
+                </View>
+
             </View>
+
+            <View style={styles.separacion}>
+                <ImageBackground
+                    source={img2}
+                    rezideMode= 'cover'
+                    imageStyle={styles.splashImage}
+                    style={styles}
+                >
+                    <Button title='Ver detalles' onPress={mostrarAlerta}/>
+                </ImageBackground>
+
+                <View style={styles.descripcion}>
+                    <Text>Descripcion descriptible</Text>
+                </View>
+
+            </View>
+
+            <View style={styles.separacion}>
+                <ImageBackground
+                    source={img2}
+                    rezideMode= 'cover'
+                    imageStyle={styles.splashImage}
+                    style={styles}
+                >
+                    <Button title='Ver detalles' onPress={mostrarAlerta}/>
+                </ImageBackground>
+
+                <View style={styles.descripcion}>
+                    <Text>Descripcion descriptible</Text>
+                </View>
+
+            </View>
+
+            <View style={styles.separacion}>
+                <ImageBackground
+                    source={img2}
+                    rezideMode= 'cover'
+                    imageStyle={styles.splashImage}
+                    style={styles}
+                >
+                    <Button title='Ver detalles' onPress={mostrarAlerta}/>
+                </ImageBackground>
+
+                <View style={styles.descripcion}>
+                    <Text>Descripcion descriptible</Text>
+                </View>
+
+            </View>
+
+            <View style={styles.separacion}>
+                <ImageBackground
+                    source={img2}
+                    rezideMode= 'cover'
+                    imageStyle={styles.splashImage}
+                    style={styles}
+                >
+                    <Button title='Ver detalles' onPress={mostrarAlerta}/>
+                </ImageBackground>
+
+                <View style={styles.descripcion}>
+                    <Text>Descripcion descriptible</Text>
+                </View>
+
+            </View>
+
+            <View style={styles.separacion}>
+                <ImageBackground
+                    source={img2}
+                    rezideMode= 'cover'
+                    imageStyle={styles.splashImage}
+                    style={styles}
+                >
+                    <Button title='Ver detalles' onPress={mostrarAlerta}/>
+                </ImageBackground>
+
+                <View style={styles.descripcion}>
+                    <Text>Descripcion descriptible</Text>
+                </View>
+
+            </View>
+
+          
+
+            
+
           
         </View>
     )
@@ -56,21 +143,46 @@ export default function MiGaleriaScreen() {
 }
 
 const styles = StyleSheet.create({
+    cuerpo:{
+       
+        
+    },
+
+    descripcion:{
+        padding: 20
+
+    },
+
+    descripcion_texto:{
+        color: 'white',
+        fontWeight: 'bold',
+
+    },
+
     splashImage:{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+ 
+        
         
         
     },
 
     image:{
+        flex: 1,
 
     },
 
     separacion:{
-        marginBottom: 20
-    }
+        marginBottom: 20,
+
+    },
+
+    botones:{
+
+    },
+
 
 });
 
